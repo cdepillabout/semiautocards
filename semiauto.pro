1dd241f5d95cd0a72390ab1d068b1e25ca205d89
(cdep)illabout <cdep.illabout@gmail.com>
2013-08-10 15:23:58 +0900

    Added stuff for translation.

---
+++ semiauto.pro
@@ -0,0 +1,18 @@
+
+# source files (.py files)
+SOURCES += semiautocards.py
+SOURCES += semiauto/mainwindow.py
+
+# forms (.ui files)
+FORMS += ui/preferences.ui
+FORMS += ui/mainwindow.ui
+
+# resources (.qrc files)
+RESOURCES += ui/resources.qrc
+
+# translations (.ts files)
+TRANSLATIONS += ui/translation.ts
+TRANSLATIONS += ui/whatwhat.ts
+
+
+
---
+++ ui/translation.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="25"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="30"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="40"/>
+        <source>Accent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="73"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="92"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="98"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="108"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="113"/>
+        <source>&amp;Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>preferencesDialog</name>
+    <message>
+        <location filename="ui/preferences.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.ui" line="71"/>
+        <source>GroupBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.ui" line="120"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.ui" line="87"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.ui" line="92"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
