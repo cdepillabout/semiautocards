3c9d1846550e51acb31afa600414af8972bb0d4e
(cdep)illabout <cdep.illabout@gmail.com>
2013-08-10 16:15:28 +0900

    temp commit. how do i actually use the translations.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# This generates the .ts files
+pylupdate4 semiauto.pro
+
+# this generates the .qm files
+lrelease-qt4 semiauto.pro
+
+# TODO: 
+# HOW TO ACTUALLY GET THE PROGRAM TO USE MY TRANSLATIONS
+
 pyuic4 ui/preferences.ui -o semiauto/gen/preferences_ui.py
 pyuic4 ui/mainwindow.ui -o semiauto/gen/mainwindow_ui.py
 pyrcc4 ui/resources.qrc -o semiauto/gen/resources_rc.py
--- ui/translations/ja_JP.ts
+++ ui/translations/ja_JP.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP" sourcelanguage="en_US">
 <context>
     <name>MainWindow</name>
     <message>
@@ -20,7 +21,7 @@
     <message>
         <location filename="ui/forms/mainwindow.ui" line="40"/>
         <source>Accent</source>
-        <translation type="unfinished"></translation>
+        <translation>高低アクセント</translation>
     </message>
     <message>
         <location filename="ui/forms/mainwindow.ui" line="73"/>
@@ -30,22 +31,22 @@
     <message>
         <location filename="ui/forms/mainwindow.ui" line="92"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>ファイル(&amp;F)</translation>
     </message>
     <message>
         <location filename="ui/forms/mainwindow.ui" line="98"/>
         <source>&amp;Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>設定(&amp;S)</translation>
     </message>
     <message>
         <location filename="ui/forms/mainwindow.ui" line="108"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>終了(&amp;Q)</translation>
     </message>
     <message>
         <location filename="ui/forms/mainwindow.ui" line="113"/>
         <source>&amp;Preferences...</source>
-        <translation type="unfinished"></translation>
+        <translation>オプション(&amp;P)...</translation>
     </message>
 </context>
 <context>
